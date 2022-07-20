7c01540bbd7708add3bd2f87fe7f11c6752299dd
sohail <sohail@sohails-MacBook-Air.local>
2022-07-20 12:26:20 +0530

    make some changes

--- lib/screen3.dart
+++ lib/screen3.dart
@@ -19,196 +19,196 @@ class _Screen3State extends State<Screen3> {
     return DefaultTabController(
         length: 2,
         child: Scaffold(
-          body: SingleChildScrollView(
-
-            child: SafeArea(
-
-              child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          InkWell(onTap:(){
-            Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => Screen2()));
-
-            },
-                            child: Icon(
-                              Icons.arrow_back_ios,
-                              size: 20,
-                              color: Colors.grey,
-                            ),
-                          ),
-
-                      SizedBox(
-                        height: 30,
+            bottomNavigationBar: BottomNavigationBar(
+                currentIndex: _currentIndex,
+                onTap: (index) => setState(() => _currentIndex = index),
+                items: [
+                  BottomNavigationBarItem(
+                      icon: Icon(
+                        Icons.arrow_drop_down_circle,
+                        color: Colors.grey,
+                        size: 40,
                       ),
-                      Column(
+                      label: '',
+                      backgroundColor: Colors.white),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.thumb_up, color: Colors.grey, size: 40),
+                      label: 'like'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.message_sharp,
+                          color: Colors.grey, size: 40),
+                      label: 'massage'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.person_rounded,
+                          color: Colors.grey, size: 40),
+                      label: 'contect')
+                ]),
+            body: SingleChildScrollView(
+              child: SafeArea(
+                child: Column(children: [
+                  Padding(
+                      padding: const EdgeInsets.all(20.0),
+                       child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              StringConstant.tellusbitaboutyou,
-                              style: TextStyle(
-                                  fontSize: 33, fontWeight: FontWeight.w500),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Text(
-                              StringConstant.name,
-                              style: TextStyle(
-                                fontSize: 20,
+                            InkWell(
+                              onTap: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => Screen2()));
+                              },
+                              child: Icon(
+                                Icons.arrow_back_ios,
+                                size: 20,
+                                color: Colors.grey,
                               ),
                             ),
                             SizedBox(
-                              height: 10,
+                              height: 30,
                             ),
-                            TextField(
-                                decoration: InputDecoration(
-                                    hintText: StringConstant.name,
-                                    hintStyle: TextStyle(fontSize: 20),
-                                    border: OutlineInputBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(10)))),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Text(
-                              StringConstant.dateofbirth,
-                              style: TextStyle(
-                                fontSize: 20,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            TextField(
-                              decoration: InputDecoration(
-                                  hintText: StringConstant.dateofbirth,
-                                  suffixIcon: Icon(Icons.calendar_today,
-                                      color: Colors.grey[300]),
-                                  hintStyle: TextStyle(fontSize: 20),
-                                  border: OutlineInputBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(10))),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Text(
-                              StringConstant.Gender,
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            SizedBox(height: 10,),
-                            Container(
-                              height: 60,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  border: Border.all(width: 2)),
-                              child: Padding(
-                                padding: const EdgeInsets.all(5.0),
-                                child: TabBar(
-                                    unselectedLabelColor: Colors.redAccent,
-                                    indicatorSize: TabBarIndicatorSize.tab,
-                                    indicator: BoxDecoration(
-                                        borderRadius:
-                                            BorderRadius.circular(10),
-                                        color: Colors.cyanAccent[200]),
-                                    tabs: [
-                                      Tab(
-                                        child: Row(
-                                          children: [
-                                            Icon(Icons.male,
-                                                size: 30,
-                                                color: Colors.black),
-                                            SizedBox(
-                                              width: 10,
-                                            ),
-                                            Text(StringConstant.male,
-                                                style: TextStyle(
-                                                    fontSize: 20,
-                                                    color: Colors.black)),
-                                          ],
-                                        ),
-                                      ),
-                                      Tab(
-                                        child: Row(
-                                          children: [
-                                            Icon(Icons.female,
-                                                size: 30,
-                                                color: Colors.black),
-                                            Align(
-                                              alignment: Alignment.center,
-                                              child: Text(
-                                                  StringConstant.female,
-                                                  style: TextStyle(
-                                                      color: Colors.black,
-                                                      fontSize: 20)),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ]),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 130,
-                            ),
-                            InkWell(onTap: (){
-                              Navigator.of(context).push(
-                                  MaterialPageRoute(builder: (context) => Screen4()));
-
-                            },
-                              child: Container(
-                                height: 60,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(25),
-                                    border: Border.all(
-                                        color: Colors.deepOrangeAccent,
-                                        width: 2)),
-                                child: Center(
-                                    child: Text(
-                                  StringConstant.nextstep,
-                                  style: TextStyle(
+                            Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    StringConstant.tellusbitaboutyou,
+                                    style: TextStyle(
+                                        fontSize: 33,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Text(
+                                    StringConstant.name,
+                                    style: TextStyle(
                                       fontSize: 20,
-                                      color: Colors.deepOrangeAccent,
-                                      fontWeight: FontWeight.bold),
-                                )),
-                              ),
-                            ),
-
-                          ]),
-                    ])),
-
-                      BottomNavigationBar(currentIndex: _currentIndex,onTap: (index)=> setState(()=>_currentIndex = index),
-                          items: [
-                            BottomNavigationBarItem(
-                                icon: Icon(
-                                  Icons.arrow_drop_down_circle,
-                                  color: Colors.grey,
-                                  size: 40,
-                                ),
-                                label: '',
-                                backgroundColor: Colors.white),
-                            BottomNavigationBarItem(
-                                icon: Icon(Icons.thumb_up,
-                                    color: Colors.grey, size: 40),
-                                label: 'like'),
-                            BottomNavigationBarItem(
-                                icon: Icon(Icons.message_sharp,
-                                    color: Colors.grey, size: 40),
-                                label: 'massage'),
-                            BottomNavigationBarItem(
-                                icon: Icon(Icons.person_rounded,
-                                    color: Colors.grey, size: 40),
-                                label: 'contect')
-                          ])
-
-
-
-           ] ),
-          ),
-          ) ));
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  TextField(
+                                      decoration: InputDecoration(
+                                          hintText: StringConstant.name,
+                                          hintStyle: TextStyle(fontSize: 20),
+                                          border: OutlineInputBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(10)))),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Text(
+                                    StringConstant.dateofbirth,
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  TextField(
+                                    decoration: InputDecoration(
+                                        hintText: StringConstant.dateofbirth,
+                                        suffixIcon: Icon(Icons.calendar_today,
+                                            color: Colors.grey[300]),
+                                        hintStyle: TextStyle(fontSize: 20),
+                                        border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10))),
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Text(
+                                    StringConstant.Gender,
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Container(
+                                    height: 60,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        border: Border.all(width: 2)),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(5.0),
+                                      child: TabBar(
+                                          unselectedLabelColor:
+                                              Colors.redAccent,
+                                          indicatorSize:
+                                              TabBarIndicatorSize.tab,
+                                          indicator: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                              color: Color(0xFF79e9fc)),
+                                          tabs: [
+                                            Tab(
+                                              child: Row(
+                                                children: [
+                                                  Icon(Icons.male,
+                                                      size: 30,
+                                                      color: Colors.black),
+                                                  SizedBox(
+                                                    width: 10,
+                                                  ),
+                                                  Text(StringConstant.male,
+                                                      style: TextStyle(
+                                                          fontSize: 20,
+                                                          color: Colors.black)),
+                                                ],
+                                              ),
+                                            ),
+                                            Tab(
+                                              child: Row(
+                                                children: [
+                                                  Icon(Icons.female,
+                                                      size: 30,
+                                                      color: Colors.black),
+                                                  Align(
+                                                    alignment: Alignment.center,
+                                                    child: Text(
+                                                        StringConstant.female,
+                                                        style: TextStyle(
+                                                            color: Colors.black,
+                                                            fontSize: 20)),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ]),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 130,
+                                  ),
+                                  InkWell(
+                                    onTap: () {
+                                      Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                              builder: (context) => Screen4()));
+                                    },
+                                    child: Container(
+                                      height: 60,
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(25),
+                                          border: Border.all(
+                                              color: Colors.deepOrangeAccent,
+                                              width: 2)),
+                                      child: Center(
+                                          child: Text(
+                                        StringConstant.nextstep,
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            color: Colors.deepOrangeAccent,
+                                            fontWeight: FontWeight.bold),
+                                      )),
+                                    ),
+                                  ),
+                                ]),
+                          ])),
+                ]),
+              ),
+            )));
   }
 }
--- lib/screen4.dart
+++ lib/screen4.dart
@@ -33,82 +33,12 @@ class _Screen4State extends State<Screen4> {
     {'title': 'Tennis'},
     {'title': 'Trainspotting'},
     {'title': 'Yoga'},
-
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(children: [
-        Padding(
-      padding: const EdgeInsets.all(20.0),
-      child:
-          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        InkWell(onTap:(){
-          Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => Screen3()));
-
-        },
-          child: Icon(
-            Icons.arrow_back_ios,
-            size: 20,
-            color: Colors.grey,
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Center(
-            child: Text(
-          StringConstant.addhobbies,
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
-        )),
-        SizedBox(
-          height: 20,
-        ),
-        Text(
-          StringConstant.Search,
-          style: TextStyle(fontSize: 20),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        TextField(
-          decoration: InputDecoration(
-              labelText: StringConstant.searchforhobbies,
-              prefixIcon: Icon(Icons.search_sharp, size: 30),
-              labelStyle: TextStyle(fontSize: 20),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-              )),
-        ),
-        SizedBox(
-          height: 15,
-        ),
-        Container(
-          height: 455,
-          width: double.infinity,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5,vertical: 5),
-            child: GridView.count(
-              crossAxisCount: 2,
-              mainAxisSpacing: 20,
-              crossAxisSpacing: 20,
-              childAspectRatio: 3,
-              children: List.generate(item.length, (index) {
-                print('listlength:${item[index]['title']}');
-                return _buildCardViewWidget(
-                  title: item[index]['title'],
-                );
-              }),
-            ),
-          ),
-        ),
-
-      ]),
-        ),
-        BottomNavigationBar(
+        bottomNavigationBar: BottomNavigationBar(
             currentIndex: _currentIndex,
             onTap: (index) => setState(() => _currentIndex = index),
             items: [
@@ -124,16 +54,86 @@ class _Screen4State extends State<Screen4> {
                   icon: Icon(Icons.thumb_up, color: Colors.grey, size: 40),
                   label: 'like'),
               BottomNavigationBarItem(
-                  icon: Icon(Icons.message_sharp,
-                      color: Colors.grey, size: 40),
+                  icon: Icon(Icons.message_sharp, color: Colors.grey, size: 40),
                   label: 'massage'),
               BottomNavigationBarItem(
-                  icon: Icon(Icons.person_rounded,
-                      color: Colors.grey, size: 40),
+                  icon:
+                      Icon(Icons.person_rounded, color: Colors.grey, size: 40),
                   label: 'contect')
-            ])
-      ]),
-    ));
+            ]),
+        body: SingleChildScrollView(
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => Screen3()));
+                      },
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        size: 20,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Center(
+                        child: Text(
+                      StringConstant.addhobbies,
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                    )),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      StringConstant.Search,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: StringConstant.searchforhobbies,
+                          prefixIcon: Icon(Icons.search_sharp, size: 30),
+                          labelStyle: TextStyle(fontSize: 20),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          )),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      height: 500,
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 5, vertical: 5),
+                        child: GridView.count(
+                          crossAxisCount: 2,
+                          mainAxisSpacing:20,
+                          crossAxisSpacing: 20,
+                          childAspectRatio: 4,
+                          children: List.generate(item.length, (index) {
+                            print('listlength:${item[index]['title']}');
+                            return _buildCardViewWidget(
+                              title: item[index]['title'],
+                            );
+                          }),
+                        ),
+                      ),
+                    ),
+                  ]),
+            ),
+          ]),
+        ));
   }
 
   _buildCardViewWidget({
@@ -141,12 +141,13 @@ class _Screen4State extends State<Screen4> {
   }) {
     return Container(
       decoration: BoxDecoration(
-         borderRadius: BorderRadius.circular(10),
-        color: Colors.white,border: Border.all(width: 2)
-      ),
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          border: Border.all(width: 2)),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Text(title,style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold)),
+        child: Text(title,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
       ),
     );
   }
